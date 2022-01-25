3832721db0d85771c293b925419e9ea5803fc885
Cahit Arslan <cahitarslan.business@gmail.com>
2022-01-25 23:26:35 +0300

    categories operations created

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
+import { ListResponseModel } from '../models/listResponseModule';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  apiUrl = 'https://localhost:44314/api/categories/getall';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getCategories(): Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
+}
