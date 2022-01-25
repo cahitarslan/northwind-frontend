a06734a840312504f13370c142743a728e7fbb86
Cahit Arslan <cahitarslan.business@gmail.com>
2022-01-26 00:25:14 +0300

    from category to products routing added

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -8,7 +8,8 @@ import { CategoryService } from 'src/app/services/category.service';
   styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-  categories: Category[] = [];
+  categories: Category[];
+  currentCategory: Category;
 
   constructor(private categoryService: CategoryService) {}
 
@@ -20,5 +21,29 @@ export class CategoryComponent implements OnInit {
     this.categoryService.getCategories().subscribe((response) => {
       this.categories = response.data;
     });
+  }
+
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+
+  clearCurrentCategory() {
+    this.currentCategory = { categoryId: 0, categoryName: '' };
+  }
+
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
+
+  getAllCategoryClass() {
+    if (!this.currentCategory || this.currentCategory.categoryId == 0) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
   }
 }
